ced37bca703aeeb22f78ef8fda8aa7a48940c999
laurapellizari <pellizarilaura@gmail.com>
2022-01-19 13:50:45 -0300

    First commit

---
+++ Tratamento_Dados/rename_fire_images.py
@@ -0,0 +1,37 @@
+import re
+import os
+import shutil
+
+#O Dataset for Forest Fire Detection, o qual foi escolhido
+# como padrão de dados, classifica como:
+# nofire_0000
+# fire_0000
+
+
+#Adicionar o caminho correspondente ao diretório a ser mudado
+main_folder = r'C:\Users\laura\Desktop\fire_dataset\fire_images'
+
+def rename_file(file):
+  file_name, file_extension = os.path.splitext(file)
+  file_name_numbers = re.findall(r'\d+', file_name)
+
+  if not file_name_numbers:
+    return file
+
+  file_name_numbers = file_name_numbers[0].zfill(4)
+
+  return f'fire_{file_name_numbers}{file_extension}'
+
+
+def main_loop():
+  for root, dirs, files in os.walk(main_folder):
+    for file in files:
+      new_file_name = rename_file(file)
+      old_file_full_path = os.path.join(root, file)
+      new_file_full_path = os.path.join(root, new_file_name)
+
+      shutil.move(old_file_full_path, new_file_full_path)
+
+
+if __name__ == '__main__':
+  main_loop()
